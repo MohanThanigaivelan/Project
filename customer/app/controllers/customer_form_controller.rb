2753477244ebccd4affc072af6524f46bcb805e7
MohanThanigaivelan <mohan@rently.com>
2018-10-09 18:56:39 +0530

    create read display operation and phone number validation

--- customer/app/controllers/customer_form_controller.rb
+++ customer/app/controllers/customer_form_controller.rb
@@ -1,4 +1,8 @@
 class CustomerFormController < ApplicationController
+   def index
+    @customer = CustomerForm.all
+  end
+
   def show
     @customer = CustomerForm.find(params[:id])
   end
@@ -8,9 +12,20 @@ class CustomerFormController < ApplicationController
   def create
   # render plain: params[:customer_form].inspect
    @customer = CustomerForm.new(customer_params)
-  	 @customer.save
-  	 redirect_to @customer
+  
+  if @customer.save
+    redirect_to @customer
+  else
+    render 'new'
   end
+  end
+
+def destroy
+  @customer = CustomerForm.find(params[:id])
+  @customer.destroy
+ 
+  redirect_to customer_form_index_path
+end
   private
   def customer_params
     params.require(:customer_form).permit(:name, :DOB,:Address,:Phone_no,:Photo)
--- customer/app/models/customer_form.rb
+++ customer/app/models/customer_form.rb
@@ -1,2 +1,4 @@
 class CustomerForm < ApplicationRecord
+	validates :Phone_no, presence: true,
+                    length: { minimum: 10 }
 end
